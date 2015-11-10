e35bb60ec573c13b992dea346a86949ec28a8cdf
lucasduarte <lucasduarte599@gmail.com>
2015-11-10 16:33:29 -0200

    Config email user

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb'
 load(app_env_vars) if File.exists?(app_env_vars)
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["USER"],
+  :user_name => ENV["SG_USER"],
   :password => ENV["PASSWORD"],
   :address => 'smtp.sendgrid.net',
   :port => 587,
